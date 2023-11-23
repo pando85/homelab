ed2ccbbb4b8e37ba7164c02241d1fd4b729eeea4
Alexander Gil <pando855@gmail.com>
2023-11-23 18:45:23 +0100

    hass: Add input boolean for enable appdaemon-climate

--- apps/home-assistant/appdaemon/climate/hello.py
+++ apps/home-assistant/appdaemon/climate/hello.py
@@ -1,13 +1,7 @@
 import appdaemon.plugins.hass.hassapi as hass
 
-#
-# Hello World App
-#
-# Args:
-#
 
 class HelloWorld(hass.Hass):
-
-  async def initialize(self):
-    self.log("Hello from AppDaemon")
-    self.log("You are now ready to run Apps!")
+    async def initialize(self):
+        self.log("Hello from AppDaemon")
+        self.log("You are now ready to run Apps!")
