1157a307caa57a3024535c6204139dc041533976
Alexander Gil <pando855@gmail.com>
2024-03-20 07:17:49 +0100

    hass: Add last connection sensor app to appdaemon and fix service monitor

---
+++ apps/home-assistant/appdaemon/apps/last_connection_sensor.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+import appdaemon.plugins.hass.hassapi as hass
+
+
+class LastConnectionSensor(hass.Hass):
+    async def initialize(self):
+        time = datetime.time(0, 0, 0)
+        self.run_minutely(self.update_last_connection, time)
+
+    async def update_last_connection(self, cb_args):
+        now = datetime.now()
+        self.set_state("sensor.last_appdaemon_connection", state=now.strftime("%Y-%m-%d %H:%M:%S"))
