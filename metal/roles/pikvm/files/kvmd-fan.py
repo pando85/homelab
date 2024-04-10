03305086778474e018f76ec40760cfb3db567347
Alexander Gil <pando855@gmail.com>
2024-04-10 21:08:05 +0200

    metal: Add socket to kvmd-fan in pikvm

--- metal/roles/pikvm/files/kvmd-fan.py
+++ metal/roles/pikvm/files/kvmd-fan.py
@@ -3,12 +3,19 @@
 
 import RPi.GPIO
 import time
+import socket
+import os
+import json
+import logging
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from socketserver import UnixStreamServer
 
 #settings
 fan_gpio=12 # Fan GPIO pin number (default: 12)
 idle_speed = 25 # Fan speed when under min_temp(IDLE) (min: 0, max: 100)
 min_temp = 45 # Fan starting temperature
 max_temp = 60 # Fan max speed temperature (max: 60)
+socket_path = '/run/kvmd/fan.sock' # UNIX socket path
 
 #define GPIO
 RPi.GPIO.setwarnings(False)
@@ -17,33 +24,73 @@ RPi.GPIO.setup(fan_gpio, RPi.GPIO.OUT)
 pwm = RPi.GPIO.PWM(fan_gpio,100)
 running = False
 
+# setup logging
+logging.basicConfig(level=logging.INFO)
+
+set_speed = None
+cpu_temp = None
+
+def calculate_fan_speed(cpu_temp):
+    if cpu_temp >= max_temp:
+        return 100
+    else:
+        temp_speed = int((cpu_temp - min_temp) / (max_temp - min_temp) * 100)
+        return min(max(idle_speed, temp_speed), 100)
+
+class MyHandler(BaseHTTPRequestHandler):
+    def address_string(self):
+        try:
+            return super().address_string()
+        except IndexError:
+            return 'unix_socket'
+
+    def do_GET(self):
+        logging.info("GET %s", self.path)
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps({"ok": True, "result": {"version": "1.0"}}).encode())
+        elif self.path == '/state':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps({"ok": True, "result": {"temp": cpu_temp, "fan_speed": set_speed}}).encode())
+        else:
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(b'Not found\n')
+
 #running code
 try:
-	while True:
-		tmpFile = open( '/sys/class/thermal/thermal_zone0/temp' )
-		cpu_temp = int(tmpFile.read())/1000
-		tmpFile.close()
-		print("Temp:",cpu_temp)
-		if (cpu_temp >= max_temp) or (cpu_temp > min_temp):
-			if(not running): #if fan not running, start fan
-				print("PWM_Start")
-				pwm.start(0)
-				running = True
-			if (cpu_temp >= max_temp): #For safety, temp exceeds 60 degrees, the fan speed is always set to 100%.
-				set_speed = 100
-			else:
-				temp_speed = int((cpu_temp - min_temp) / (max_temp - min_temp) * 100) # set 0~100% for min and max temp ranges
-				set_speed = min(max(idle_speed, temp_speed), 100) # make speed not over 100
-			print("set_speed:",set_speed,"%")
-			pwm.ChangeDutyCycle(set_speed)
-		else :
-			if(running):
-				running = False
-				pwm.stop()
-				print("PWM_Stop")
+    if os.path.exists(socket_path):
+        os.remove(socket_path)
+    with UnixStreamServer(socket_path, MyHandler) as httpd:
+        while True:
+            try:
+                with open('/sys/class/thermal/thermal_zone0/temp') as tmpFile:
+                    cpu_temp = int(tmpFile.read())/1000
+                logging.info("Temp: %s", cpu_temp)
+                if cpu_temp >= min_temp:
+                    if not running:
+                        logging.info("PWM_Start")
+                        pwm.start(0)
+                        running = True
+                    set_speed = calculate_fan_speed(cpu_temp)
+                    logging.info("set_speed: %s %%", set_speed)
+                    pwm.ChangeDutyCycle(set_speed)
+                else:
+                    if running:
+                        running = False
+                        pwm.stop()
+                        logging.info("PWM_Stop")
+            except Exception as e:
+                logging.error("Error: %s", e)
 
-		time.sleep(5)
+            time.sleep(1)
+
+            httpd.handle_request()
 
 except KeyboardInterrupt:
-		pass
+    pass
 pwm.stop()
