9c91a0eab6581d5847e8a12eb739aeeaaa912cc2
Alexander Gil <pando855@gmail.com>
2025-06-21 01:56:54 +0200

    hass: Adjust datetime with timezone in appdaemon last connection

--- apps/home-assistant/appdaemon/apps/last_connection_sensor.py
+++ apps/home-assistant/appdaemon/apps/last_connection_sensor.py
@@ -9,5 +9,5 @@ class LastConnectionSensor(hass.Hass):
         self.run_minutely(self.update_last_connection, time)
 
     async def update_last_connection(self, cb_args):
-        now = datetime.datetime.now()
+        now = datetime.now(self.get_timezone())
         self.set_state("sensor.last_appdaemon_connection", state=now.strftime("%Y-%m-%d %H:%M:%S"))
